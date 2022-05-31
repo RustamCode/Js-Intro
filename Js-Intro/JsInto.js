ab1df356d3bf66ff9437dfbbf58e61a1cba9cb98
RustamCode <100156400+RustamCode@users.noreply.github.com>
2022-05-31 18:12:47 +0400

    upload

---
+++ Js-Intro/JsInto.js
@@ -0,0 +1,35 @@
+//task 1
+var a = 20;
+var b = 2;
+
+sum = a/b ;
+
+console.log(sum);
+
+//task2
+A = "Ha";
+a = A.repeat(2);
+console.log(a);
+
+//task3
+
+ total( [1, 2, 3, 4]);
+
+function total(arr){
+    var a = 0
+ for (let i = 0; i < arr.length; i++) {
+    a = a + arr[i]
+     
+ }
+    console.log(a);
+}
+
+//task4
+
+function sahe(a,b)
+ {
+
+   return a*b;
+
+}
+console.log(sahe(6,70));
